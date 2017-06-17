f5a5d749a807b7cf5eb510ebd36b1729c600e1a0
SVvitaly <vv-wolg@mail.ru>
2017-06-17 21:35:51 +0300

    fixed problem with gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,3 +31,6 @@ set :repo_url, "git@github.com:SVvitaly/RCOP.git"
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
